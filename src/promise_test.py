37b660b85eaeffa10dd4135993caa847f5446224
Seulgi Kim <dev@seulgi.kim>
2016-03-29 01:52:32 +0900

    Implement Promise.
    Python's Future is too hard to use. Promise is helper class to use Promise
    easily.

---
+++ src/promise_test.py
@@ -0,0 +1,25 @@
+from .promise import Promise
+import unittest
+
+
+class PromiseTest(unittest.TestCase):
+    @unittest.expectedFailure
+    def test_failed(self):
+        Promise.failed(Exception).f.result()
+
+    def test_successful(self):
+        value = 1
+        result = Promise.successful(value).f.result()
+        self.assertEqual(value, result)
+
+    def test_apply(self):
+        result = Promise.apply(lambda: 1).f.result()
+        self.assertEqual(1, result)
+
+    @unittest.expectedFailure
+    def test_apply_returns_failed_promise_when_exception_raised(self):
+        Promise.apply(raise_exception).result()
+
+
+def raise_exception():
+    raise Exception()
