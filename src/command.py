e2c9c83e44c5f110aad69f7adcf332db9ba03bc0
Seulgi Kim <dev@seulgi.kim>
2016-03-29 03:25:20 +0900

    Implement NEW command.
    This command returns new key to client.
    If client passes key, server validates the key and return new key if not valid.

--- src/command.py
+++ src/command.py
@@ -3,6 +3,7 @@ from .promise import Promise
 from enum import Enum
 from typing import List
 from typing import Tuple
+import json
 
 
 class Command(Enum):
@@ -12,7 +13,25 @@ class Command(Enum):
     def get(message: str) -> Promise:
         return Promise.apply(lambda: _get_command(message))
 
+    @staticmethod
+    def handle(command: 'command.Command', argv: List[str]) -> Promise:
+        if (command == Command.NEW):
+            return Promise.apply(lambda: _handle_new(*argv)).map(
+                lambda key: json.dumps({'key': key})
+            )
+        else:
+            return Promise.failed(LookupError('invalid command'))
+
 
 def _get_command(message: str) -> Tuple[Command, List[str]]:
     command, argv = message.split(':', 1)
     return (Command[command], argv.split(':'))
+
+
+def _handle_new(*argv: List[str]) -> str:
+    if len(argv) == 0:
+        return Connection.instance().new()
+    elif len(argv) == 1:
+        return Connection.instance().get_or_new(argv[0])
+    else:
+        raise LookupError()
--- src/command_handler.py
+++ src/command_handler.py
@@ -1,3 +1,7 @@
+from .command import Command
+from tornado.ioloop import IOLoop
+import json
+import tornado.concurrent
 import tornado.websocket
 
 
@@ -6,7 +10,25 @@ class CommandHandler(tornado.websocket.WebSocketHandler):
         pass
 
     def on_message(self, message):
-        self.write_message(message)
+        p = Command.get(message)
+        x = p.f.result()
+        result = p.flatMap(_handle_command)
+        IOLoop.instance().add_future(
+            result.f, lambda result: self.send_response(result))
 
     def on_close(self):
         pass
+
+    def send_response(self, result):
+        try:
+            response = result.result()
+            print('RESPONSE:' + response)
+            self.write_message(response)
+        except Exception as e:
+            print('ERROR:' + str(e))
+            self.write_message(e)
+
+
+def _handle_command(commands):
+    command, args = commands
+    return Command.handle(command, args)
