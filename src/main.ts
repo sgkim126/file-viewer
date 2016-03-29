ab749d9ffb860148f88d3d06123cdb82099a2f07
Seulgi Kim <dev@seulgi.kim>
2016-03-30 02:20:23 +0900

    Make connection on page loading

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
 import render from './render.tsx';
+import Connection from './connection.ts';
 import './viewer.styl';
 
-((document: Document): void => {
+((window: Window, document: Document): void => {
   const main: HTMLDivElement = document.getElementById('main') as HTMLDivElement;
+  const USER_KEY = 'user-key';
+
+  const connection = Connection.open(localStorage.getItem(USER_KEY));
+  connection.then((connection) => {
+    localStorage.setItem(USER_KEY, connection.key);
+  });
   render(main);
-})(window.document);
+})(window, window.document);
