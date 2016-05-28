4449b153f7c3f2ae3f05cd5b405dc2a8d4a9a21d
Seulgi Kim <dev@seulgi.kim>
2016-05-28 20:35:06 +0900

    Make user select view file or directory.

--- client/file-browser.tsx
+++ client/file-browser.tsx
@@ -7,6 +7,7 @@ import Menu from './menu.tsx';
 import CommandOption from './options.ts';
 import Panel from './panel.tsx';
 import Path from './path.tsx';
+import { Button, ButtonGroup } from 'react-bootstrap';
 import { ICommandInput } from './messages.ts';
 
 interface IProps {
@@ -25,6 +26,8 @@ interface IState {
     y?: number,
   };
   menu?: IMenu;
+  file_visible?: boolean;
+  dir_visible?: boolean;
 }
 
 export default class FileBrowser extends React.Component<IProps, IState> {
@@ -36,11 +39,12 @@ export default class FileBrowser extends React.Component<IProps, IState> {
         y: 0,
       },
       menu: null,
+      file_visible: true,
+      dir_visible: true,
     };
   }
 
   public render(): JSX.Element {
-    let files = <div>'Loading...'</div> as any;
     let selectedFile = { } as any;
     const onContextMenu = (eventX: number, eventY: number, file: IFile) => {
       const x = eventX - this.state.position.x;
@@ -48,25 +52,39 @@ export default class FileBrowser extends React.Component<IProps, IState> {
       const menu = {x, y, file};
       this.setState({ menu });
     };
-    if (this.props.files) {
-      files = this.props.files.map((file: IFile, key: number) => {
-        if (key === this.state.selected) {
-          selectedFile = file;
-        }
-        const selected = key === this.state.selected;
-        return <File
-          dirpath={this.props.path}
-          selected={selected}
-          key={key}
-          {...file}
-          onContextMenu={onContextMenu}
-          onClick={(e: React.MouseEvent) => this.props.onClick(e, `${this.props.path}/${file.name}`, file.is_file)}
-        />;
-      });
-    }
+    const filter = (file: IFile) => {
+      return (file.is_dir && this.state.dir_visible)
+        || (file.is_file && this.state.file_visible);
+    };
+    const files = this.props.files.filter(filter).map((file: IFile, key: number) => {
+      if (key === this.state.selected) {
+        selectedFile = file;
+      }
+      const selected = key === this.state.selected;
+      return <File
+      dirpath={this.props.path}
+      selected={selected}
+      key={key}
+      {...file}
+      onContextMenu={onContextMenu}
+      onClick={(e: React.MouseEvent) => this.props.onClick(e, `${this.props.path}/${file.name}`, file.is_file)}
+      />;
+    });
     const menu = this.state.menu ? this.menu() : <div></div>;
+    const onFileVisible = (e: React.MouseEvent) => {
+      e.preventDefault();
+      this.setState({file_visible: !this.state.file_visible});
+    };
+    const onDirVisible = (e: React.MouseEvent) => {
+      e.preventDefault();
+      this.setState({dir_visible: !this.state.dir_visible});
+    };
     return <Panel title='files' onStop={this.onStop.bind(this)}>
       <Path changeDir={this.changeDir.bind(this)} home={this.props.home}>{this.props.path}</Path>
+      <ButtonGroup justified>
+        <Button bsStyle='primary' href='#' onClick={onFileVisible} active={this.state.file_visible}>File</Button>
+        <Button bsStyle='primary' href='#' onClick={onDirVisible} active={this.state.dir_visible}>Directory</Button>
+      </ButtonGroup>
       <div className='file-browser'>
         {files}
       </div>
